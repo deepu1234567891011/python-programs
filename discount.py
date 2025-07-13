0dcbaff1aa42f51a7c406d3a1f772a4f7838dbbb
vicharapu srideepika <deepikavicharapu@gmail.com>
2025-07-13 09:21:56 +0530

    Update discount.py

--- discount.py
+++ discount.py
@@ -1,20 +1,14 @@
 def Check(amount):
-    if(amount>=50000):
+    if(amount>5000):
         discount=5
-    elif(amount>=10000):
+    elif(amount>10000):
         discount=10
-    elif(amount>=20000):
-        disount=20
+    elif(amount>20000):
+        discount=20
     else:
         discount=0
-        discount=amount*(discount/100)
-        final_amount=amount-discount 
-        if discount==5:
-            print("5%discount",discount)
-        elif discount==10:
-            print("10%discount",discount)
-        elif discount==20:
-            print("20%discount",discount)
-        print("final amount is:",final_amount)    
-check=(9000)
-    
+    discount=amount*(discount/100)
+    final_amount=amount-discount 
+    print("final discount is:",final_amount)
+Check(9000)
+            
