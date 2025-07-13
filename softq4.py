a658a5147cd09c1cda3fd09dee85b7ae1be71771
vicharapu srideepika <deepikavicharapu@gmail.com>
2025-07-13 09:30:24 +0530

    Add files via upload

---
+++ softq4.py
@@ -0,0 +1,11 @@
+a=(input("enter 6 digits number:"))
+d1=int(a[0])
+d2=int(a[1])
+d3=int(a[2])
+d4=int(a[3])
+d5=int(a[4])
+d6=int(a[5])
+sum1=d1+d3+d5
+sum2=d2+d4+d6
+print(sum1,"odd")
+print(sum2,"even")
