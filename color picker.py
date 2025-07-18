a3cb472cf3485a33489334eb231a146206926e29
vicharapu srideepika <deepikavicharapu@gmail.com>
2025-07-18 14:07:21 +0530

    Add files via upload

---
+++ color picker.py
@@ -0,0 +1,20 @@
+import random
+color_names=['red','green','blue','yellow','pink','orange']
+color=random.choice(color_names)#computer selecting colour names randomly
+#print available colors
+print("available colors are:",color_names)
+# taking loop for user choice 4 times i will ask
+for attempt in range(4):
+    user=input("enter a your guess of color:").strip().lower()#user enter the color randomly
+    #if user enter color and computer color are mathed it will print winner other wise else stament
+    if user==color:
+        print("hey your winner keep going deepu!!")
+        break
+    else:
+        print("wron please try again....")
+else:
+    print("computer choice is",color)
+
+    
+
+
